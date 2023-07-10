81eb9029de10e149787f728f6363c562626c2945
hrutik7 <62372737+hrutik7@users.noreply.github.com>
2023-07-10 17:32:53 +0530

    formatted code

--- src/components/EngagementHelper.js
+++ src/components/EngagementHelper.js
@@ -1,94 +1,65 @@
-import Highcharts from 'highcharts';
+import Highcharts from "highcharts";
 
 const engagementHelper = {
-    engagementMessageOverTimeChartOptions: (messageCountList, channels) => {
-      // Filter channels that have messages for more than 1 date
-      // const filteredChannels = channels.filter(channel =>
-      //  {
-      //   console.log(channel,"messageCountList[channel].length")
-      //   return messageCountList[channel]?.length > 1
-      //  }
-      // );
-      console.log(messageCountList.length,"channels.length")
-      const filteredChannels = channels.filter(channel => channel.messages );
-      // console.log(filteredChannels,"filteredChannels")
-  
-      // Generate the series data for each channel
-      // const seriesData = filteredChannels.map(channel => {
-      //   const data = messageCountList[channel]?.map(date => ({
-      //     x: new Date(date.date).getTime(), // Assuming the date property is available in each item
-      //     y: date.count,
-      //   }));
-      const seriesData = filteredChannels.map(channel => {
-       console.log(messageCountList.map(date =>  parseInt(new Date(date.timeBucket).getUTCDate())),"messageCountList")
-        const data = messageCountList.map(date => 
-       
-          (
-            {
-              
-              
+  engagementMessageOverTimeChartOptions: (messageCountList, channels) => {
+    console.log(messageCountList.length, "channels.length");
+    const filteredChannels = channels.filter((channel) => channel.messages);
 
-              x: new Date(date.timeBucket).getTime(), // Assuming the date property is available in each item
-              //right now the distance between x axis is 7 days do it 1 day
-              // x: new Date(date.timeBucket).getUTCDate(), // Assuming the date property is available in each item
-              y: Number(date.count),
-            }
-          )
-        
-     
-        
-        );
-      
-        return { name: channel, data };
-      });
-  console.log(seriesData,"seriesData")
-      // Define the chart options
-      const options = {
-        chart: {
-          type: 'line',
+    const seriesData = filteredChannels.map((channel) => {
+      console.log(
+        messageCountList.map((date) =>
+          parseInt(new Date(date.timeBucket).getUTCDate())
+        ),
+        "messageCountList"
+      );
+      const data = messageCountList.map((date) => ({
+        x: new Date(date.timeBucket).getTime(),
+
+        y: Number(date.count),
+      }));
+
+      return { name: channel, data };
+    });
+    console.log(seriesData, "seriesData");
+
+    const options = {
+      chart: {
+        type: "line",
+      },
+      title: {
+        text: "Engagement Messages Over Time",
+      },
+
+      xAxis: {
+        type: "datetime",
+        dateTimeLabelFormats: {
+          day: "%e of %b",
         },
         title: {
-          text: 'Engagement Messages Over Time',
+          text: "Date",
         },
-        // xAxis: {
-        //   type: 'datetime',
-        //   title: {
-        //     text: 'Date',
-        //   },
-        // },
-        //do x axis format in day and month
-        xAxis: {
-          type: 'datetime',
-          dateTimeLabelFormats: {
-            day: '%e of %b',
-          },
-          title: {
-            text: 'Date',
-          }, 
+      },
+      yAxis: {
+        title: {
+          text: "Message Count",
         },
-        yAxis: {
-          title: {
-            text: 'Message Count',
-          },
-        },
-        
-        // tooltip: {
-        //   shared: false,
-        //   crosshairs: false,
-        // },
-        //onhover show the date and count
+      },
 
-        tooltip: {
-          formatter: function() {
-            return  Highcharts.dateFormat('%e - %b', this.x) +': '+ this.y +' messages';
-          }
+      tooltip: {
+        formatter: function () {
+          return (
+            Highcharts.dateFormat("%e - %b", this.x) +
+            ": " +
+            this.y +
+            " messages"
+          );
         },
-        series: seriesData,
-      };
-  // console.log(options,"options")
-      return options;
-    },
-  };
-  
-  export default engagementHelper;
-  
+      },
+      series: seriesData,
+    };
+
+    return options;
+  },
+};
+
+export default engagementHelper;
